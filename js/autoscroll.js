fb44195cac8f4f5028d3d27fa5d536c84b41dbfe
Ty Kelly <kellytyquan@gmail.com>
2016-07-28 05:32:32 -0400

    Increase autoscroll speed and clean up code structure

--- js/autoscroll.js
+++ js/autoscroll.js
@@ -1,5 +1,7 @@
 jQuery(document).ready(function($){
 
+	var $window = $(window);
+
 	//checks if current page matches parameter
 	// parameter must be a regular expression
 	function pageIs(page){
@@ -8,7 +10,7 @@ jQuery(document).ready(function($){
 
 	//focus window on given element
 	function objectInView(div, margin){
-		return $(window).scrollTop( $( div ).offset().top - margin );
+		return $window.scrollTop( $(div).offset().top - margin );
 	}
 
 	/* Autoscrolls page in same direction that user is scrolling to the page's nearest project preview image. */
@@ -16,13 +18,13 @@ jQuery(document).ready(function($){
 	//scrolls top of window to given element
 	//add distance between top of window & elem with 2nd param
 	function scroll_to(div, margin){
-		$( "html, body" ).stop().animate({
+		$("html, body").stop().animate({
 			scrollTop: $(div).offset().top - margin
-		},1400);
+		},1000);
 	}
 
 	//work page & 1024px min-width window
-	if ( pageIs(/work.html/) && $( window ).width() >= 1024 )
+	if ( pageIs(/work.html/) && $window.width() >= 1024 )
 	{
 		//array of class selectors representing each project
 		var projects = [".prg1", ".prg2", ".prg3", ".prg4", ".prg5"];
@@ -32,7 +34,7 @@ jQuery(document).ready(function($){
 		objectInView(".prg1", 100);
 
 
-		$( window ).bind( "mousewheel DOMMouseScroll", function(event){
+		$window.bind( "mousewheel DOMMouseScroll", function(event){
 			//scrolling up & not at top of docuemnt
 	    if ( projects[i-1] && event.originalEvent.wheelDelta > 0 || event.originalEvent.detail < 0 )
 	    {
@@ -45,7 +47,7 @@ jQuery(document).ready(function($){
 	    	if ( projects[i+1] )
 	    	{
 	    		i++;
-	    		scroll_to( projects[i], 100 );
+	    		scroll_to(projects[i], 100);
 	    	}
 
 	    }
--- js/projects.js
+++ js/projects.js
@@ -1,7 +1,12 @@
 jQuery(document).ready(function($) {
 
+	var $window = $(window);
+	var $site_view_sltr = $("#site_view");
+	var $site_view_text = $("a #site_view p");
+	var $site_view_link = $("a #site_view");
+
 	//position of site_view selector
-	const site_view_pos = topOfObject("#site_view");
+	const SITE_VIEW_POS = topOfObject("#site_view");
 
 	//checks if current page matches parameter
 	// parameter must be a regular expression
@@ -10,7 +15,7 @@ jQuery(document).ready(function($) {
 	}
 
 	function topOfObject(obj){
-		return $( obj ).offset().top;
+		return $(obj).offset().top;
 	}
 
 	//all project pages
@@ -19,13 +24,13 @@ jQuery(document).ready(function($) {
 		/* Sticks site view button to page header and fixes to window as user scrolls past button. Returns button to original position once passed in opposite direction. */ 
 
 		//while scrolling page
-		$( window ).bind("mousewheel DOMMouseScroll", function(e){
+		$window.bind("mousewheel DOMMouseScroll", function(){
 			
 			//header passes below site_view selector
-			if ( $( window ).scrollTop() > site_view_pos - 60 )
+			if ( $window.scrollTop() > SITE_VIEW_POS - 60 )
 			{
 				//stick site_view to header & add transparent colo to bg 
-				$( "#site_view" ).css({
+				$site_view_sltr.css({
 					"z-index": "97",
 					"position": "fixed",
 					"margin-top": "60px",
@@ -33,44 +38,44 @@ jQuery(document).ready(function($) {
 				});
 
 				//change site_view text to white
-				$("a #site_view p").css("color", "#FFF");
+				$site_view_text.css("color", "#FFF");
 
 				//keep same css styles for hovering
-				$( "a #site_view" ).hover(function(){
+				$site_view_link.hover(function(){
 
-					var new_bgclr = $("a #site_view").css("background-color").replace("0.4", "1");
+					var $new_bgclr = $site_view_link.css("background-color").replace("0.4", "1");
 					
-					$("a #site_view").css("background-color", new_bgclr);
+					$site_view_link.css("background-color", $new_bgclr);
 				});
 
-				$("a #site_view").mouseleave(function(){
-					$("a #site_view").css("background-color", $(".svbg_clr").css("background-color"));
+				$site_view_link.mouseleave(function(){
+					$site_view_link.css( "background-color", $(".svbg_clr").css("background-color") );
 				});
 
 			}
 			//header passes above site_view return to default css
-			else if ( $( window ).scrollTop() < site_view_pos && $("#site_view").css("position") == "fixed" )
+			else if ( $window.scrollTop() < SITE_VIEW_POS && $site_view_sltr.css("position") == "fixed" )
 			{
-				$("#site_view").css({
+				$site_view_sltr.css({
 					"position": "absolute",
 					"background-color": "",
 					"margin-left": "auto",
 					"margin-right": "auto",
 					"margin-bottom": "0",
-					"margin-top": site_view_pos
+					"margin-top": SITE_VIEW_POS
 				});
 
-				$("a #site_view p").css("color", "#222222");
+				$site_view_text.css("color", "#222222");
 
-				$( "a #site_view" ).hover(function(){
+				$site_view_link.hover(function(){
 
-					$("a #site_view").css( "background-color", $(".svbg_clr").css("background-color").replace("0.4", "1") );
-					$("a #site_view p").css("color", "#FFF");
+					$site_view_link.css( "background-color", $(".svbg_clr").css("background-color").replace("0.4", "1") );
+					$site_view_text.css("color", "#FFF");
 				});
 
-				$("a #site_view").mouseleave(function(){
-					$("a #site_view").css("background-color", "");
-					$("a #site_view p").css("color", "#222");
+				$site_view_link.mouseleave(function(){
+					$site_view_link.css("background-color", "");
+					$site_view_text.css("color", "#222");
 				});
 
 			}
@@ -78,13 +83,14 @@ jQuery(document).ready(function($) {
 			/* Fixes content to window while user scrolls between the 2nd and 3rd analysis background. */
 
 			//only infiniti & wun project pages & 800px min-width window
-			if ( $(window).width() >= 800 && (pageIs(/infiniti/) || pageIs(/wun/) ) )
+			if ( $window.width() >= 800 && ( pageIs(/infiniti/) || pageIs(/wun/) ) )
 			{
 				//scrolling from analysis_bg2 to analysis_bg3
-				if ( $( window ).scrollTop() > topOfObject( "#analysis_bg2" ) - 60 && $(window).scrollTop() + 240 + $(".content2 p").outerHeight() + $(".content_title2 p").outerHeight() < topOfObject( "#analysis_bg3" ) )
+				if ( $window.scrollTop() > topOfObject("#analysis_bg2") - 60 && $window.scrollTop() + 240 + 
+					$(".content2 p").outerHeight() + $(".content_title2 p").outerHeight() < topOfObject("#analysis_bg3") )
 				{
 					//fix position of content to window 
-					$(".content2").add(".content_title2").css({
+					$(".content2, .content_title2").css({
 						"position": "fixed",
 						"top": "50px"
 					});
@@ -93,7 +99,7 @@ jQuery(document).ready(function($) {
 				else
 				{
 					//return content to default
-					$(".content2").add(".content_title2").css({
+					$(".content2, .content_title2").css({
 						"position": "static",
 						"top": "0"
 					});	
