581890f9136f2b93345b42244a6b9e02b9b08d1a
Ty Kelly <kellytyquan@gmail.com>
2016-12-05 21:12:30 -0500

    fix issue with name on nav overflowing vertically on mobile devices

--- js/nav.js
+++ js/nav.js
@@ -1,34 +1,76 @@
 jQuery(document).ready(function($) {
 
 	// Hides nav button and displays nav menu.
-	$(".mobile_nav_button").click(function() {
-		$(this).hide();
-		$("#nav").css("display", "block");
+	$(document).click(function(e) {
+		if ( e.target.id == "mobile_nav" ) 
+		{
+			$(".mobile_nav_button").hide();
+			$("#nav").css("display", "block");
 			$("#nav").animate({
 				"width":"100%"
 			}, 1500);
+		}
+
+		// Hides nav menu and displays nav button.
+		if ( e.target.id == "close_btn" || e.target.id != "nav" )
+		{
+			if ( $("#nav").css("width") == "250px" )
+			{
+				$("#nav").animate({
+					"width":"0"
+				}, 500);
+
+				setTimeout(function() {
+					$("#nav").css("display", "none");
+				}, 500);
+
+				$(".mobile_nav_button").show()
+			}
+		}
 	});
 
-	// Hides nav menu and displays nav button.
-	$("#close_btn").click(function() {
-		$("#nav").animate({
-			"width":"0"
-		}, 500);
+	//checks if current page matches parameter
+	// parameter must be a regular expression
+	function pageIs(page){
+		return page.test(location.href);
+	}
 
-		setTimeout(function() {
-			$("#nav").css("display", "none");
-		}, 500);
-		$(".mobile_nav_button").show()
-	});
+	// Represents top of a given object within document
+	function topOfObject(obj){
+		return $(obj).offset().top;
+	}
 
-	// Changes nav button to red while being hovered
-	$(".mobile_nav_button").on("mouseenter", function() {
-		$(this).attr("src", "images/mobile_nav_button_red.png");
-	});
+	// Represents bottom of a given object within document
+	function bottomOfObject(obj) {
+		return $(obj).offset().top + $(obj).outerHeight();
+	}
 
-	// Changes nav button back to white once stopped hovering
-	$(".mobile_nav_button").on("mouseout", function() {
-		$(this).attr("src", "images/mobile_nav_button_white.png");
-	});
+	if ( pageIs(/project_/) )
+	{
+		$(window).bind("mousewheel DOMMouseScroll", function() {
+
+			// If within intro site analysis area
+			if ( $(window).scrollTop() > topOfObject("#analysis_container") - 70 && $(window).scrollTop() < bottomOfObject("#analysis_container") - 70 )
+			{
+				// Transition button to black
+				$(".mobile_nav_button").css("transition", "1.5s");
+				$(".mobile_nav_button").addClass("mobile_nav_button_black");
+
+				setTimeout(function() {
+					$(".mobile_nav_button").css("transition", "0s");
+				}, 500);
+			}
+			else
+			{
+				// // Transition button to white
+				$(".mobile_nav_button").css("transition", "1.5s");
+				$(".mobile_nav_button").removeClass("mobile_nav_button_black");
+
+				setTimeout(function() {
+					$(".mobile_nav_button").css("transition", "0s");
+				}, 500);
+			}
+		});
+	}
 	
 });
