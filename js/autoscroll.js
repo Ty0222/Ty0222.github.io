6acbea0491c7f28105eb5be18385e467e3679846
Ty Kelly <kellytyquan@gmail.com>
2016-12-05 21:08:26 -0500

    redesign work page and improve auto-scrolling

--- js/autoscroll.js
+++ js/autoscroll.js
@@ -1,53 +1,79 @@
 jQuery(document).ready(function($){
 
-	var $window = $(window);
-
-	//checks if current page matches parameter
-	// parameter must be a regular expression
+	// Checks if current page matches parameter
+	// Parameter must be a regular expression
 	function pageIs(page){
 		return page.test(location.href);
 	}
 
-	//focus window on given element
-	function objectInView(div, margin){
-		return $window.scrollTop( $(div).offset().top - margin );
+	function scrollingUp(e) {
+		return e.originalEvent.wheelDelta > 0 || e.originalEvent.detail < 0
 	}
 
-	/* Autoscrolls page in same direction that user is scrolling to the page's nearest project preview image. */
-
-	//scrolls top of window to given element
-	//add distance between top of window & elem with 2nd param
-	function scroll_to(div, margin){
-		$("html, body").stop().animate({
-			scrollTop: $(div).offset().top - margin
-		},1000);
+	function scrollingDown(e) {
+		return e.originalEvent.wheelDelta < 0 || e.originalEvent.detail > 0
 	}
 
-	//work page & 1024px min-width window
-	if ( pageIs(/work.html/) && $window.width() >= 1024 )
+	function topOfObject(obj) {
+		return $(obj).offset().top;
+	}
+
+	function bottomOfObject(obj) {
+		return $(obj).offset().top + $(obj).outerHeight();
+	}
+
+	function topOfWindow() {
+		return $(window).scrollTop();
+	}
+
+	function bottomOfWindow() {
+		return $(window).scrollTop() + $(window).outerHeight();
+	}
+
+	// Array of class selectors representing each project
+	var projects = [".prg1", ".prg2", ".prg3", ".prg4", ".prg5"];
+	var i = 0;
+
+	// 1024px min-width window
+	if ( $(window).width() >= 768 )
 	{
-		//array of class selectors representing each project
-		var projects = [".prg1", ".prg2", ".prg3", ".prg4", ".prg5"];
-		var i = 0;
 
-		//focus window on first project 
-		objectInView(".prg1", 100);
+		// On scroll
+		$(window).bind("mousewheel DOMMouseScroll", function(e){
 
-
-		$window.bind( "mousewheel DOMMouseScroll", function(event){
-			//scrolling up & not at top of docuemnt
-	    if ( projects[i-1] && event.originalEvent.wheelDelta > 0 || event.originalEvent.detail < 0 )
+			// Scrolling up & not at top of document
+	    if ( projects[i-1] && scrollingUp(e) )
 	    {
-	        i--;
-	        scroll_to( projects[i], 100 );
+	    	// Hide all project titles
+	    	$(".work_preview_text").each(function(){
+	    		$(this).css("opacity","0");
+	    	});
+
+	    	i--;
+
+	    	// Autoscroll to next project
+	    	$("html, body").animate({ scrollTop: $(projects[i]).offset().top }, 800);
+
+	    	// Fade in project title text
+	    	$(projects[i]).children(".work_preview_text").animate({"opacity":"1"}, 1600);
 	    }
-	    //scrolling down & not at bottom of document
+	    // Scrolling down & not at bottom of document
 	    else
 	    {
 	    	if ( projects[i+1] )
 	    	{
+	    		// Hide all project titles
+	    		$(".work_preview_text").each(function(){
+	    			$(this).css("opacity","0");
+	    		});
+
 	    		i++;
-	    		scroll_to(projects[i], 100);
+
+	    		// Autoscroll to next project
+	    		$("html, body").animate({ scrollTop: $(projects[i]).offset().top }, 800);
+
+	    		// Fade in project title text
+	    		$(projects[i]).children(".work_preview_text").animate({"opacity":"1"}, 1600);
 	    	}
 
 	    }
@@ -56,34 +82,88 @@ jQuery(document).ready(function($){
 
 	}
 
-	//swaps given images when hovering over element
-	//maintains consistent styling when hovering elem text
-	function imgSwap(klass, img1, img2){
-		$("." + klass).on("mouseenter", function(){
-			$("." + klass).attr("src", "images/" + img1);
-			$("#work_preview_text_" + klass).css("color", "#FFF");
-		});
+	// 414px max-width window
+	if ( $(window).width() <= 414 )
+	{
+		// On scroll
+		$(window).bind("mousewheel DOMMouseScroll", function(e){
 
-		$("#work_preview_text_" + klass).on("mouseenter", function(){
-			$("." + klass).attr("src", "images/" + img1);
-			$("#work_preview_text_" + klass).css("color", "#FFF");
-		});
+			// Iterate through each text element
+			$(".work_preview_text").each(function(){
 
-		$("." + klass).on("mouseout", function(){
-			$("." + klass).attr("src", "images/" + img2);
-			$("#work_preview_text_" + klass).css("color", "#CCC");
-		});
+				// When text is within view
+				if ( scrollingDown(e) && bottomOfObject(this) < bottomOfWindow() )
+				{
+					$(this).animate({"opacity":"1"}, 75);
 
+					// Set index to last when at bottom of page
+					if ( $(this).attr("id") == $(projects[projects.length-1]).children("a").attr("id") )
+					{
+						i = projects.length-1
+					}
+				}
+				
+				// When text is going out of view
+				if ( scrollingUp(e) && bottomOfObject(this) > topOfWindow() + 100 )
+				{
+					$(this).css("opacity", "0");
+				}
+
+				// Reset index when at top of page
+				if ( topOfWindow() < 130 )
+				{
+					i = 0;
+				}
+				
+			});
+
+		});
 	}
 
-	imgSwap("blueprint", "work_blueprint.jpg", "work_blueprint_covered.jpg");
+	// Guiding arrow to help identify the number of projects left
+	// to scroll to
+	$(window).bind("mousewheel DOMMouseScroll", function(e){
 
-	imgSwap("flixer", "work_flixer.jpg", "work_flixer_covered.jpg");
+		// Hide arrow when over footer
+		if ( bottomOfObject(".arrow") > topOfObject("#footer") - 4 )
+    {
+    	$(".arrow").css("opacity", "0");
+    }
+    else
+    {
+    	$(".arrow").css("opacity", "1");	
+    }
+		
+		// Display upward arrow when last project is within view
+    if ( projects[i] == projects[projects.length-1] )
+    {
+    	$(".arrow").addClass("arrow_up");
+    	$(".arrow").removeClass("arrow_down");
+    }
 
-	imgSwap("realitiy", "work_realitiy.jpg", "work_realitiy_covered.jpg");
+    // Display downward arrow when first project is within view
+    if ( projects[i] == projects[0] )
+    {
+    	$(".arrow").addClass("arrow_down");
+    	$(".arrow").removeClass("arrow_up");
+    }
+	});
+	
+	//toggles toggle_class for target_class when hovering over hover_class
+	function indirectToggleClass(hover_class, target_class, toggle_class){
+		$("." + hover_class).on("mouseenter", function(){
+			$("." + target_class).addClass(toggle_class);
+		});
 
-	imgSwap("infiniti", "work_infiniti.jpg", "work_infiniti_covered.jpg");
-
-	imgSwap("wun", "work_wun.jpg", "work_wun_covered.jpg");
+		$("." + hover_class).on("mouseout", function(){
+			$("." + target_class).removeClass(toggle_class);
+		});
+	}
+	
+	indirectToggleClass("blueprint_text", "blueprint_covered", "blueprint");
+	indirectToggleClass("flixer_text", "flixer_covered", "flixer");
+	indirectToggleClass("realitiy_text", "realitiy_covered", "realitiy");
+	indirectToggleClass("infiniti_text", "infiniti_covered", "infiniti");
+	indirectToggleClass("wun_text", "wun_covered", "wun");
 	
 });
